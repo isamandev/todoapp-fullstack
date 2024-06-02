9b48b6399c7182a5b93ca30c10c98fa8ae1abf3e
SamanAbbasi <samanabbasi.plus@gmail.com>
2024-06-02 11:07:01 +0330

    complete todo app

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,15 +1,46 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import styles from "./todo.module.css";
 function Todo() {
   const [todoName, setTodoName] = useState("");
   const [todoList, setTodoList] = useState([]);
 
+  useEffect(() => {
+    axios.get("http://localhost:8000/api/todo").then((result) => {
+      const todoData = result.data;
+      const todos = [];
+
+      for (const key in todoData) {
+        todos.push({ id: key, title: todoData[key].title });
+      }
+
+      setTodoList(todos);
+    });
+  }, []);
+
   const inputChangeHandler = (event) => {
     setTodoName(event.target.value);
   };
+
   const todoAddHandler = (event) => {
     event.preventDefault();
-    setTodoList((pre) => [...pre, todoName]);
+    setTodoList((pre) => [
+      ...pre,
+      { id: todoList.length + 1, title: todoName },
+    ]);
+    console.log(todoList);
+
+    axios
+      .post("http://localhost:8000/api/todo", {
+        title: todoName,
+      })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
     setTodoName("");
   };
   return (
@@ -30,8 +61,8 @@ function Todo() {
       </form>
       <ul className={styles.list}>
         {todoList.map((item) => (
-          <li className={styles["list-item"]} key={item}>
-            {item}
+          <li className={styles["list-item"]} key={item.id}>
+            {item.title}
           </li>
         ))}
       </ul>
